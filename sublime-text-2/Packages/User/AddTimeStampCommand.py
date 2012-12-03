11d54e1f44bec97610ed7638906910fb639581b0
Rayzen <r4y23n@gmail.com>
2012-12-03 03:32:47 +0100

    Date plugin for snippet

--- sublime-text-2/Packages/User/AddTimeStampCommand.py
+++ sublime-text-2/Packages/User/AddTimeStampCommand.py
@@ -7,6 +7,5 @@ class AddTimeStampCommand(sublime_plugin.TextCommand):
     def run(self, edit):
     	now = datetime.datetime.now()
     	# return standard SOA formatted serial for today
-    	print self.view.sel()[0]
     	date = now.strftime("%Y%m%d00")
     	self.view.insert(edit,self.view.sel()[0].begin(),date)
