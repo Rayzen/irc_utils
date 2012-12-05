099627feb8c24c201b8577cce9808b7042897af6
Rayzen <r4y23n@gmail.com>
2012-12-05 21:28:18 +0100

    bgp and rip snippets added

--- netkit/utils/lab.sh
+++ netkit/utils/lab.sh
@@ -15,7 +15,7 @@ start_bind(){
     for m in $hws; do
         if [[ -f $m.startup ]] && [[ -d $m ]]; then
             echo "" >> $m.startup
-            echo "/etc/init.d/bind start" >> $m.startup
+            echo "#\n/etc/init.d/bind start" >> $m.startup
             mkdir -p $m/etc/bind
             touch $m/etc/bind/named.conf
         fi
@@ -26,26 +26,60 @@ start_bind(){
 start_apache(){
 	local srv="$@"
 	local curdir=`pwd`
-	local dest="/home/guest/public_html/index.html"
+	local dest="home/guest/public_html/index.html"
 	for s in $srv; do
-		if [[ -f $m.startup ]] && [[ -d $m ]]; then
-			echo "\na2enmod userdir\n/etc/init.d/apache2 start" >> $s.startup
+		if [[ -f $s.startup ]] && [[ -d $s ]]; then
+			echo "#\na2enmod userdir\n/etc/init.d/apache2 start" >> $s.startup
 			touchp $s/$dest
 			echo "<html><body>Hello! This is $s speaking!!!</" >> $dest
 		fi
 	done
 }
 
-start_rip(){
-	#TODO
+start_zebra(){
+	local router="$@"
+	local dest="etc/zebra"
+	for r in $router; do
+		if [[ -f $r.startup ]] && [[ -d $r ]]; then
+			echo "#\n/etc/init.d/zebra start"
+			touchp $r/$dest/zebra.conf
+			touchp $r/$dest/daemons
+			echo "Zebra activated on $r"
+		fi
+	done	
 }
 
-start_ospf(){
-	#TODO
+start_rip(){
+	local router="$@"
+	local dest="etc/zebra/ripd.conf"
+	start_zebra $router
+	for r in $router; do
+		if [[ -f $r.startup ]] && [[ -d $r ]]; then
+			touchp $r/$dest
+		fi
+	done
+}
+
+start_ospf(){	
+	local router="$@"
+	local dest="etc/zebra/ospfdd.conf"
+	start_zebra $router
+	for r in $router; do
+		if [[ -f $r.startup ]] && [[ -d $r ]]; then
+			touchp $r/$dest
+		fi
+	done
 }
 
 start_bgp(){
-	#TODO
+	local router="$@"
+	local dest="etc/zebra/bgpd.conf"
+	start_zebra $router
+	for r in $router; do
+		if [[ -f $r.startup ]] && [[ -d $r ]]; then
+			touchp $r/$dest
+		fi
+	done
 }
 
 open_lab(){
