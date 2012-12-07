656425889d11ffbdb9134fc9819198366506bce3
Rayzen <r4y23n@gmail.com>
2012-12-07 14:25:56 +0100

    Solarized xterm and some fix on setup.sh

---
+++ netkit/env/netkit-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Netkit configuration file
+
+export NETKIT_HOME="/home/antonio/netkit"
+export PATH=$PATH:$NETKIT_HOME/bin
+export MANPATH=$MANPATH:$NETKIT_HOME/man
--- setup.sh
+++ setup.sh
@@ -3,13 +3,9 @@
 dir=`pwd`
 echo $dir
 ln -s $dir/netkit $HOME/netkit-util
+ln -s $dir/netkit/env/colors/Xresources $HOME/.Xresources
 cp -f $dir/sublime-text-2/Packages/Color\ Scheme\ -\ Default/* $HOME/.config/sublime-text-2/Packages/Color\ Scheme\ -\ Default
 cp -f $dir/sublime-text-2/Packages/User/* $HOME/.config/sublime-text-2/Packages/User
-
-
-echo
-echo "Solarizing terminal"
-sh $dir/netkit/env/gnome-terminal-colors-solarized-master/set_dark.sh
 
 echo
 echo "add \$HOME/bin to PATH"
@@ -21,13 +17,20 @@ echo "export PATH=$PATH:$HOME/netkit-util/Sublime\ Text\ 2"  >> $HOME/.bashrc
 
 echo 
 echo "Add bash script to .bashrc"
+echo "#Scripts" >> $HOME/.bashrc
 echo "source $HOME/netkit-util/utils/lab.sh" >> $HOME/.bashrc
 echo "source $HOME/netkit-util/utils/util.sh" >> $HOME/.bashrc
 
 echo
-echo "Add aliases to .bashrc"
-echo "alias ls='ls --color=auto -l"
-echo "alias tree='tree -C"
+echo "Add aliases to .bashrc" 
+echo "#Aliases" >> $HOME/.bashrc
+echo "alias ls='ls --color=auto -l" >> $HOME/.bashrc
+echo "alias tree='tree -C">> $HOME/.bashrc
+
+echo
+echo "Solarizing terminal"
+sh $dir/netkit/env/colors/gnome-terminal-colors-solarized-master/set_dark.sh
+echo "xrdb -load $HOME/.Xresources" >> $HOME/.bashrc
 
 echo
 echo "Loading new settings"
