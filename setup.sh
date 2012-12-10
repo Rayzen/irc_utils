3ddd3e08f5a4378339f8a0435924906aea8358bf
Antonio Zoccoli <r4y23n@gmail.com>
2012-12-10 12:06:32 +0100

    simpleswitcher autoload

--- setup.sh
+++ setup.sh
@@ -5,18 +5,18 @@ echo $dir
 ln -sf $dir/netkit $HOME/netkit-util
 ln -sf $dir/netkit/env/colors/Xresources $HOME/.Xresources
 
-cp -f $dir/sublime-text-2/Packages/Color\ Scheme\ -\ Default/* $HOME/.config/sublime-text-2/Packages/Color\ Scheme\ -\ Default
-cp -f $dir/sublime-text-2/Packages/User/* $HOME/.config/sublime-text-2/Packages/User
-
 echo
 echo "add \$HOME/bin to PATH"
 echo "export PATH=\$PATH:\$HOME/netkit-util/bin" >> $HOME/.bashrc
 
+###Start sublime###
 sublime_text &
 sublime_pid=$!
 echo $sublime_pid
 kill -9 $sublime_pid
 
+cp -f $dir/sublime-text-2/Packages/Color\ Scheme\ -\ Default/* $HOME/.config/sublime-text-2/Packages/Color\ Scheme\ -\ Default
+cp -f $dir/sublime-text-2/Packages/User/* $HOME/.config/sublime-text-2/Packages/User
 
 echo 
 echo "Add bash script to .bashrc"
@@ -28,12 +28,15 @@ echo
 echo "Add aliases to .bashrc" 
 echo "#Aliases" >> $HOME/.bashrc
 echo "alias ls='ls --color=auto -l" >> $HOME/.bashrc
-echo "alias tree='tree -C">> $HOME/.bashrc
 
 echo
 echo "Solarizing terminal"
 sh $dir/netkit/env/colors/gnome-terminal-colors-solarized-master/set_dark.sh
 echo "xrdb -load \$HOME/.Xresources" >> $HOME/.bashrc
+
+echo
+echo "Start simpleswitcher"
+echo "simpleswitcher 2>/dev/null &" >> $HOME/.bashrc
 
 echo
 echo "Loading new settings"
